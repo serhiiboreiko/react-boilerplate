ffd59fa12ed291ad0d0689f1586d5ccfe14e4c08
jestersunborn <jestersunborn@gmail.com>
2019-03-19 16:38:10 +0200

    Add theme support to storybook. Import global styles and fonts

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,20 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+
+// Styles components theme
+import theme from '../src/styles/theme.js';
+
+const themes = [theme];
+// Use addon so we can use styles-components theme
+addDecorator(withThemesProvider(themes));
+
+// Add global styles and fonts
+require('../src/styles/fonts.css');
+require('../src/styles/index.css');
 
 const loadStories = () => {
   require('../src/components/stories');
+  require('../src/components/stories/text');
 };
 
 configure(loadStories, module);
